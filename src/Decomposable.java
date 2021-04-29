051941e6a90f5f2f9cf5993a83f9f662298c491d
Eva Szatmari <szateva@gmail.com>
2021-04-29 20:46:24 +0100

    all 3 passed all test, decomposition is a little ugly...

--- src/Decomposable.java
+++ src/Decomposable.java
@@ -1,9 +1,30 @@
 public class Decomposable {
+    /**
+     *
+     * loop through the array, create all possible pairs from the strings in the array
+     * then check if the newly created pair is equal to the string (a) that is passed in
+     *
+     */
+    public static boolean decomposable( String[] a, String s ) {
+        int len = a.length;
+        String pair = "";
+        Boolean result = null;
+        outerloop:
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (a[i] != a[j]) {
+                    pair = a[i] + a[j];
+                    if (pair.equals(s)) {
+                        result =  true;
+                        break outerloop;
+                    } else result = false;
+                }
+            }
+        }
+        return result;
 
-    public static boolean decomposable( String[] a, String s )
-    {
-        return false;
     }
+
     public static void main(String[] args) {
         String a[] = { "pom", "pom", "mop", "ping", "pong" };
         System.out.println( decomposable( a, "mopping" ));
